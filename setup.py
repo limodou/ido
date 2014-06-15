208d90cfe484c65ebe7f0dc05f24193caaa8b45d
limodou <limodou@gmail.com>
2014-06-15 10:22:58 +0800

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,52 @@
 __doc__ = open('README.md').read()
 
 from setuptools import setup
+from setuptools.command import build_py as b
+import os
+
+def copy_dir(self, package, src, dst):
+    self.mkpath(dst)
+    for r in os.listdir(src):
+        if r in ['.svn', '_svn']:
+            continue
+        fpath = os.path.join(src, r)
+        if os.path.isdir(fpath):
+            copy_dir(self, package + '.' + r, fpath, os.path.join(dst, r))
+        else:
+            ext = os.path.splitext(fpath)[1]
+            if ext in ['.pyc', '.pyo', '.bak', '.tmp']:
+                continue
+            target = os.path.join(dst, r)
+            self.copy_file(fpath, target)
+
+def find_dir(self, package, src):
+    for r in os.listdir(src):
+        if r in ['.svn', '_svn']:
+            continue
+        fpath = os.path.join(src, r)
+        if os.path.isdir(fpath):
+            for f in find_dir(self, package + '.' + r, fpath):
+                yield f
+        else:
+            ext = os.path.splitext(fpath)[1]
+            if ext in ['.pyc', '.pyo', '.bak', '.tmp']:
+                continue
+            yield fpath
+
+def build_package_data(self):
+    for package in self.packages or ():
+        src_dir = self.get_package_dir(package)
+        build_dir = os.path.join(*([self.build_lib] + package.split('.')))
+        copy_dir(self, package, src_dir, build_dir)
+setattr(b.build_py, 'build_package_data', build_package_data)
+
+def get_source_files(self):
+    filenames = []
+    for package in self.packages or ():
+        src_dir = self.get_package_dir(package)
+        filenames.extend(list(find_dir(self, package, src_dir)))
+    return filenames
+setattr(b.build_py, 'get_source_files', get_source_files)
 
 setup(name='ido',
     version='0.1',
